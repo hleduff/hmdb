ebdc54bc44622251ae08dd4fafaa4cb861b00bfe
Hugo Le Duff <hleduff@yahoo.com>
2022-12-23 17:49:27 +0100

    config: fix simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,34 @@ module.exports = {
         browser: true,
         node: true,
         amd: true,
+        es6: true,
     },
     extends: ['plugin:react/recommended', 'airbnb-base', 'prettier'],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                paths: ['src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    plugins: ['react', 'simple-import-sort', 'import'],
     rules: {
         'react/react-in-jsx-scope': 'off',
         'react/display-name': 'off',
         'jsx-a11y/accessible-emoji': 'off',
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
         'no-console': 'warn',
         'no-plusplus': 'off',
         'no-else-return': 'off',
