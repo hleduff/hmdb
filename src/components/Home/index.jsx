7b436113e7cad45b02087cf73d816454c891b749
Hugo Le Duff <hleduff@yahoo.com>
2022-12-26 10:48:06 +0100

    feat: add basic API calls (movie list, movie page)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './style.module.css';
 
-export const Home = () => (
-    <div className={styles.root}>
-        <h1>Home page</h1>
-        <ul>
-            <li>
-                <Link to={`movie/1`}>Movie 1</Link>
-            </li>
-            <li>
-                <Link to={`movie/2`}>Movie 2</Link>
-            </li>
-        </ul>
-    </div>
-);
+export const Home = () => {
+    const [error, setError] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            `${import.meta.env.VITE_URL_API}/movie/popular?api_key=${
+                import.meta.env.VITE_API_KEY
+            }`,
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setLoaded(true);
+                    setMovies(result.results);
+                },
+                (err) => {
+                    setLoaded(true);
+                    setError(err);
+                },
+            );
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!loaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className={styles.root}>
+                <h1>Home page</h1>
+                <ul>
+                    {movies.map((movie) => (
+                        <li key={movie.id}>
+                            <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+};
--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,10 +1,43 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import styles from './style.module.css';
 
-export const Movie = () => (
-    <div className={styles.root}>
-        <h1>Movie page</h1>
-        <Link to={'/'}>&lt; Back</Link>
-    </div>
-);
+export const Movie = () => {
+    const [error, setError] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [movie, setMovie] = useState([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+        fetch(
+            `${import.meta.env.VITE_URL_API}/movie/${movieId}?api_key=${
+                import.meta.env.VITE_API_KEY
+            }`,
+        )
+            .then((res) => res.json())
+            .then(
+                (response) => {
+                    setLoaded(true);
+                    setMovie(response);
+                },
+                (err) => {
+                    setLoaded(true);
+                    setError(err);
+                },
+            );
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!loaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className={styles.root}>
+                <h1>{movie.title}</h1>
+                <Link to={'/'}>&lt; Back</Link>
+            </div>
+        );
+    }
+};
