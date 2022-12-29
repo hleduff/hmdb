2860adfa8f111998e1de3b10128c6f20911ac0aa
Hugo Le Duff <hleduff@yahoo.com>
2022-12-29 19:48:16 +0100

    rename .js(x) to .ts(x)

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './app/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
