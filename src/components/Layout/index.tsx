f00d763dbdf4b0b75355f9b2076eb2d483e0c22a
Hugo Le Duff <hleduff@yahoo.com>
2023-01-01 12:29:20 +0100

    (wip) add typing in code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,17 @@
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
+import PropTypes, { InferProps } from 'prop-types';
 
 import { Header } from '../Header';
 import styles from './style.module.css';
 
-export const Layout = ({ className, children }) => (
+const LayoutPropTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
+
+export const Layout = ({ className, children }: InferProps<typeof LayoutPropTypes>) => (
     <div className={classNames(styles.root, className)}>
         <Header />
         <main className={styles.main}>{children}</main>
     </div>
 );
-
-Layout.propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.node,
-};
--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,17 +1,37 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query';
+import type { BaseQueryFn } from '@reduxjs/toolkit/query';
 import axios from 'axios';
+import type { AxiosRequestConfig, AxiosError } from 'axios';
 
 const URL_API = import.meta.env.VITE_URL_API;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const axiosBaseQuery =
-    ({ baseUrl } = { baseUrl: '' }) =>
+    (
+        { baseUrl }: { baseUrl: string } = { baseUrl: '' },
+    ): BaseQueryFn<
+        {
+            url: string;
+            method: AxiosRequestConfig['method'];
+            data?: AxiosRequestConfig['data'];
+            params?: AxiosRequestConfig['params'];
+        },
+        unknown,
+        unknown
+    > =>
     async ({ url, method, data, params }) => {
         try {
-            const result = await axios({ url: baseUrl + url, method, data, params });
+            const result = await axios({
+                url: baseUrl + url,
+                method,
+                data,
+                params,
+            });
+
             return { data: result.data };
         } catch (axiosError) {
-            const err = axiosError;
+            const err = axiosError as AxiosError;
+
             return {
                 error: {
                     status: err.response?.status,
@@ -21,7 +41,7 @@ const axiosBaseQuery =
         }
     };
 
-export const apiSlice = createApi({
+const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: axiosBaseQuery({
         baseUrl: URL_API,
