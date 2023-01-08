3e76fd3f25469e33f701e733138408a0e41349b4
Hugo Le Duff <hleduff@yahoo.com>
2023-01-08 18:54:12 +0100

    feat(minor): better loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,34 +4,75 @@ export const Loader = () => (
     <svg
         className={styles.loader}
         xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
-        viewBox="0 0 100 100"
+        width="120"
+        height="30"
+        fill="#fff"
+        viewBox="0 0 120 30"
     >
-        <circle cx="6" cy="50" r="6" fill="#fff">
+        <circle cx="15" cy="15" r="15">
             <animate
-                attributeName="opacity"
-                begin=".1"
-                dur="1s"
+                attributeName="r"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from="15"
                 repeatCount="indefinite"
-                values="0;1;0"
+                to="15"
+                values="15;9;15"
+            />
+            <animate
+                attributeName="fill-opacity"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from="1"
+                repeatCount="indefinite"
+                to="1"
+                values="1;.5;1"
             />
         </circle>
-        <circle cx="26" cy="50" r="6" fill="#fff">
+        <circle cx="60" cy="15" r="9" fillOpacity=".3">
             <animate
-                attributeName="opacity"
-                begin=".2"
-                dur="1s"
+                attributeName="r"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from="9"
                 repeatCount="indefinite"
-                values="0;1;0"
+                to="9"
+                values="9;15;9"
+            />
+            <animate
+                attributeName="fill-opacity"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from=".5"
+                repeatCount="indefinite"
+                to=".5"
+                values=".5;1;.5"
             />
         </circle>
-        <circle cx="46" cy="50" r="6" fill="#fff">
+        <circle cx="105" cy="15" r="15">
             <animate
-                attributeName="opacity"
-                begin=".3"
-                dur="1s"
+                attributeName="r"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from="15"
                 repeatCount="indefinite"
-                values="0;1;0"
+                to="15"
+                values="15;9;15"
+            />
+            <animate
+                attributeName="fill-opacity"
+                begin="0s"
+                calcMode="linear"
+                dur="0.8s"
+                from="1"
+                repeatCount="indefinite"
+                to="1"
+                values="1;.5;1"
             />
         </circle>
     </svg>
