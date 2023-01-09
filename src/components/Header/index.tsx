e296500837de73cd6dd9e26449337fd727b66376
Hugo Le Duff <hleduff@yahoo.com>
2023-01-09 12:55:23 +0100

    feat(minor): add homepage link on header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { SelectCountry } from '../../features/locale/SelectCountry';
 
 import styles from './style.module.css';
@@ -5,7 +7,7 @@ import styles from './style.module.css';
 export const Header = () => (
     <header className={styles.header}>
         <h1 className={styles.title}>
-            HMD<span className={styles.highlight}>b</span>
+            <Link to="/popular">HMD<span className={styles.highlight}>b</span></Link>
         </h1>
         <SelectCountry />
     </header>
